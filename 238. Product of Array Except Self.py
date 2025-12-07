b390c63fe326b24dc0948e3d46cce4502e05f3d0
Kazi Rafiul Kader <kazirafi09@gmail.com>
2025-12-07 21:01:00 +0600

    new solve

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,3 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
+        pre = 1
+        post = 1
+        n = len(nums)
+        answer = [1] * n
+
+        for i in range(n):
+            answer[i] = pre
+            pre *= nums[i]
+
+        for i in range(n - 1, -1, -1):
+            answer[i] *= post
+            post *= nums[i]
+
+        return answer
