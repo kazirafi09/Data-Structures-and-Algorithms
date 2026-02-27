9a7d1013f7f45207e010b9c83ff4148ddcc99077
Kazi Rafiul Kader <kazirafi09@gmail.com>
2026-02-27 19:10:54 +0600

    New Problems Solved

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -3,4 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        fp = 0
+        for sp in range(len(nums)):
+            if nums[sp] != 0:
+                nums[fp], nums[sp] = nums[sp], nums[fp]
+                fp += 1
+        return nums
