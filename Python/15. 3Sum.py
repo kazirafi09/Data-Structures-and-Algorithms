27d0ee98de42b6bfb32c7ea68b65c16b7e5b3e58
Kazi Rafiul Kader <kazirafi09@gmail.com>
2026-06-07 23:16:17 +0600

    Solved 15

--- Python/15. 3Sum.py
+++ Python/15. 3Sum.py
@@ -1,3 +1,34 @@
-class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        total_combinations = []
+        duplicates = set()
         
+        for i in range(len(nums) - 2):
+            if nums[i] not in duplicates:
+                duplicates.add(nums[i])
+                
+                l = i + 1
+                r = len(nums) - 1
+                
+                while l < r:
+                    total = nums[i] + nums[l] + nums[r]
+                    
+                    if total > 0:
+                        r -= 1
+                    elif total < 0:
+                        l += 1
+                    else: 
+                        total_combinations.append([nums[i], nums[l], nums[r]])
+                    
+                        l += 1
+                        r -= 1
+                    
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                            
+        return total_combinations
